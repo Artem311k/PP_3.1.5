cc687b1a0a6d97eaf855f73afb324e9a2af22c6d
Artem311k <ovtrue311k@gmail.com>
2022-09-06 23:40:50 +0300

    Update

--- src/main/java/ru/kuzmin/PP_315/Application.java
+++ src/main/java/ru/kuzmin/PP_315/Application.java
@@ -3,7 +3,12 @@ package ru.kuzmin.PP_315;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/ru/kuzmin/PP_315/TestClass.java
+++ src/main/java/ru/kuzmin/PP_315/TestClass.java
@@ -29,46 +29,42 @@ public class TestClass {
 
         String url = "http://94.198.50.185:7081/api/users/";
         User user = new User(3L, "James", "Brown", (byte) 10);
-        String cookieHeader = getCookieHeader(url);
-        String firstPartOfCode = postRequest(url, cookieHeader, user);
+        HttpHeaders headers = createHeader(url);
+        String firstPartOfCode = postRequest(url, headers, user);
         user.setName("Thomas");
         user.setLastName("Shelby");
-        String secondPartOfCode = putRequest(url, cookieHeader, user);
-        String thirdPartOfCode = deleteRequest(url, cookieHeader, 3);
+        String secondPartOfCode = putRequest(url, headers, user);
+        String thirdPartOfCode = deleteRequest(url, headers, 3);
         System.out.println(firstPartOfCode + secondPartOfCode + thirdPartOfCode);
 
     }
 
-    public String getCookieHeader(String url) {
+    private HttpHeaders createHeader(String url) {
         ResponseEntity<String> rateResponse = restTemplate.exchange(url,
                 HttpMethod.GET, null, String.class);
-        HttpHeaders headers = rateResponse.getHeaders();
-        return headers.get("Set-Cookie").get(0).split(";")[0];
-    }
-
-    public String postRequest(String url, String cookieHeader, User user) {
+        HttpHeaders ResponseHeaders = rateResponse.getHeaders();
+        String cookieHeader = ResponseHeaders.get("Set-Cookie").get(0).split(";")[0];
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.set("Cookie", cookieHeader);
-        HttpEntity<User> entity = new HttpEntity<>(user,headers);
-        return restTemplate.exchange(url,
-                HttpMethod.POST, entity, String.class).getBody();
+        return headers;
     }
 
-    public String putRequest(String url, String cookieHeader, User userToUpdate) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        headers.set("Cookie", cookieHeader);
+    private String postRequest(String url, HttpHeaders headers, User user) {
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+        return restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+    }
+
+    private String putRequest(String url, HttpHeaders headers, User userToUpdate) {
         HttpEntity<User> entity = new HttpEntity<>(userToUpdate, headers);
         return restTemplate.exchange(url, HttpMethod.PUT, entity, String.class).getBody();
 
     }
 
-    public String deleteRequest(String url, String cookieHeader, int id) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        headers.set("Cookie", cookieHeader);
+    private String deleteRequest(String url, HttpHeaders headers, int id) {
         HttpEntity<User> entity = new HttpEntity<>(headers);
         return restTemplate.exchange(url + id, HttpMethod.DELETE, entity, String.class).getBody();
     }
+
+
 }
